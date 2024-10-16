cd4391d219f120c1dde651509289e9f748959be4
Shilpisingh220 <shilpibssingh27@gmail.com>
2024-10-16 13:21:59 +0000

    short  message & long message

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import data from "./data.json";
 import PokemonCard from "./components/PokemonCard";
+import {shuffle} from 'fast-shuffle';
 
 // console.log(data);
 
@@ -8,15 +9,19 @@ const pokemonRow = document.querySelector("[data-pokemon-row]");
 
 function renderPokemons(list) {
   pokemonRow.innerHTML = "";
-  for (let obj of list) {
-    const { name, image, description, link } = obj;
-    const pokemon = PokemonCard(name, image, description, link);
+  // for (let obj of list) {
+    // const { name, image, description, link } = obj;
+    // const pokemon = PokemonCard(name, image, description, link);
+
+   list.forEach((pokemonobj) => {
+    const { name, image, description, link } = pokemonobj;
+    const pokemon = PokemonCard(name, image, description, link)
 
     pokemonRow.appendChild(pokemon);
-  }
+  });
 }
 
-renderPokemons(data);
+renderPokemons(shuffle(data));
 // Filter functionality
 // Empty the previous content
 // pokemonRow.innerHTML = "";
@@ -28,7 +33,8 @@ inputEl.addEventListener("input", (e) => {
     obj.name.toLowerCase().includes(currentInput)
   );
 
-  if (filterPokemons.length === 0) {
+  // fallback pokemon card
+  if (!filterPokemons.length) {
     renderPokemons([
       {
         name: "NOT FOUND",
@@ -51,6 +57,8 @@ document.addEventListener("keyup", (event) => {
     inputEl.focus();
   }
 });
+
+renderPokemons(shuffle(data));
 
 // const inputEl = document.querySelector("#floatingInputGroup1");
 // console.log(inputEl);
