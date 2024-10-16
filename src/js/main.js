c3762806be8aa80f8b0e1fb8db2c476509cbbc88
Shilpisingh220 <shilpibssingh27@gmail.com>
2024-10-16 14:50:40 +0000

    implement fuzzle search

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,71 @@
+import { shuffle } from "fast-shuffle";
+import Fuse from "fuse.js";
+
 import data from "./data.json";
 import PokemonCard from "./components/PokemonCard";
-import {shuffle} from 'fast-shuffle';
 
-// console.log(data);
-
+// DOM Selection
 const inputEl = document.querySelector("input");
 const pokemonRow = document.querySelector("[data-pokemon-row]");
 
+// Render
 function renderPokemons(list) {
+  // Empty the previous content
   pokemonRow.innerHTML = "";
-  // for (let obj of list) {
-    // const { name, image, description, link } = obj;
-    // const pokemon = PokemonCard(name, image, description, link);
 
-   list.forEach((pokemonobj) => {
-    const { name, image, description, link } = pokemonobj;
-    const pokemon = PokemonCard(name, image, description, link)
-
+  list.forEach((pokemonObj) => {
+    const { name, image, description, link } = pokemonObj;
+    const pokemon = PokemonCard(name, image, description, link);
     pokemonRow.appendChild(pokemon);
   });
 }
 
-renderPokemons(shuffle(data));
-// Filter functionality
-// Empty the previous content
-// pokemonRow.innerHTML = "";
-inputEl.addEventListener("input", (e) => {
-  const currentInput = e.target.value.toLowerCase().trim();
-  console.log(currentInput)
-  // console.log(CurrentInput, data);
-  const filterPokemons = data.filter((obj) =>
-    obj.name.toLowerCase().includes(currentInput)
-  );
+// Filtering
+function renderFilterPokemons(input) {
+  // const filteredPokemons = data.filter((obj) =>
+  //   obj.name.toLowerCase().includes(input)
+  // );
 
-  // fallback pokemon card
-  if (!filterPokemons.length) {
+  if (!input) {
+    return renderPokemons(data);
+  }
+
+  const fuse = new Fuse(data, {
+    keys: ["name"],
+  });
+
+  const filteredPokemons = fuse.search(input).map((obj) => obj.item);
+
+  // Fallback Pokemon Card
+  if (!filteredPokemons.length) {
     renderPokemons([
       {
-        name: "NOT FOUND",
+        name: "Not Found",
         image:
           "https://i.pinimg.com/originals/11/52/0c/11520cf1cc72ad1aab32fb3f26685619.jpg",
-        description: "kuch aur type kariyo",
-        link: "https://pokemon.card",
+        description: "Try a different search term",
+        link: "https://pokemon.com",
       },
     ]);
+
     return;
   }
 
-  renderPokemons(filterPokemons);
+  renderPokemons(filteredPokemons);
+}
+
+// Listen for input
+inputEl.addEventListener("input", (e) => {
+  const currentInput = e.target.value.toLowerCase().trim();
+  renderFilterPokemons(currentInput);
 });
 
-// add keyboard functionality
+// Add keyboard functionality
 document.addEventListener("keyup", (event) => {
   if (event.key === "/") {
-    console.log(`slash was pressed`);
     inputEl.focus();
   }
 });
 
+// Inital Rendering
 renderPokemons(shuffle(data));
-
-// const inputEl = document.querySelector("#floatingInputGroup1");
-// console.log(inputEl);
